fdc07619fa323ff5b095d649335a78fd0c2cad56
josh <josh_id@yahoo.com>
2020-07-20 02:02:48 -0700

    updates

--- pdf_processor/views.py
+++ pdf_processor/views.py
@@ -1,10 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import json
 import os
 
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+
+def get_date(d: str, second_date: bool):
+    year, month, day = d.split('-')
+    months = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+    if second_date:
+        m = int(month) + 3
+        if m > 12:
+            m = m - 12
+        month = months[m]
+        year = int(year) + 1
+    else:
+        month = months[int(month)]
+    return '{} {}, {}'.format(month, int(day), year)
+
 def processor(request): 
-    pass
+
+    d = request.POST['fromDate']
+
+    first_date = get_date(d, False)
+    second_date = get_date(d, True)
+
+    packet = io.BytesIO()
+    # create a new PDF with Reportlab
+    can = canvas.Canvas(packet, pagesize=letter)
+    can.setFont('Helvetica-Bold', 9.3)
+    can.drawString(462, 122, "Smog Status:")
+    can.setFont('Helvetica', 9.3)
+    can.drawString(462, 105, "{} - {}".format(first_date, second_date))
+    can.save()
+
+    #move to the beginning of the StringIO buffer
+    packet.seek(0)
+    new_pdf = PdfFileReader(packet)
+    # read your existing PDF
+    existing_pdf = PdfFileReader(open("renewal_files/Calculate Vehicle Renewal Fees Results.pdf", "rb"))
+    output = PdfFileWriter()
+    # add the "watermark" (which is the new pdf) on the existing page
+    page = existing_pdf.getPage(0)
+    page.mergePage(new_pdf.getPage(0))
+    output.addPage(page)
+    # finally, write "output" to a real file
+    outputStream = open("renewal_files/result.pdf", "wb")
+    output.write(outputStream)
+    outputStream.close()    
+    return redirect('/')
 
 def index(request):
     return render(request, 'pdf_processor/template.html', {})
--- smog_status/urls.py
+++ smog_status/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pdf_processor.views import index
+from pdf_processor.views import index, processor
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('process/', processor)
 ]
